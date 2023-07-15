628758b7625665cc4e295152282cf966fd78000d
Manfred Araujo <manfred.araujo.2021@live.rhul.ac.uk>
2023-07-15 14:15:18 +0100

    Added a follow/unfollow button on the profile pages
    This uses a followers table to keep track of all the followers for each
    user

--- ToDoSite/explore/views.py
+++ ToDoSite/explore/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.models import User
 from post.models import post
 import json
+from account.models import follower
 
 # Create your views here.
 def explore(request):
@@ -11,4 +12,15 @@ def explore(request):
 def person(request, username):
     user_details = User.objects.get(username=username)
     Posts = post.objects.filter(user=user_details).order_by('-time')
-    return render(request, 'explore/person.html', {"user": user_details, "posts": Posts})
+    
+    if (request.method == "POST"):
+        choice = request.POST["choice"]
+        if (choice == "follow"):
+            follower.objects.create(follower=request.user, followee=user_details)
+        else:
+            follower_entry = follower.objects.filter(follower=request.user, followee=user_details)
+            if follower_entry:
+                follower_entry.delete()
+
+    is_following = follower.objects.filter(follower=request.user, followee=user_details).exists()
+    return render(request, 'explore/person.html', {"profile": user_details, "posts": Posts, "follow": is_following})
--- ToDoSite/post/models.py
+++ ToDoSite/post/models.py
@@ -1,6 +1,5 @@
 from typing import Any
 from django.db import models
-from django.conf import settings
 from django.contrib.auth.models import User
 
 # Create your models here.
