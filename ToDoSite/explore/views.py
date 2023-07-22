f407cd40772e99d05fa46cf024b68440fdfc2fe4
Manfred Araujo <manfred.araujo.2021@live.rhul.ac.uk>
2023-07-22 09:55:22 +0100

    Improved the following and follower pages
    Also setup the account image and description when they register

--- ToDoSite/explore/views.py
+++ ToDoSite/explore/views.py
@@ -5,6 +5,7 @@ import json
 from account.models import follower
 from backend.Post import Post
 from account.models import Profile
+from django.db.models import Subquery, OuterRef
 
 # Create your views here.
 def explore(request):
@@ -37,9 +38,18 @@ def person(request, username):
 def followers(request, username):
     profile = User.objects.get(username=username)
     followers = follower.objects.filter(followee=profile)
-    return render(request, 'explore/followers.html', {"profile":profile, "followers":followers})
+    follower_users = [follower.follower for follower in followers]
+    profiles = Profile.objects.filter(user__in=follower_users)
+    followers = followers.annotate(creator_profile_image=Subquery(profiles.filter(user=OuterRef('follower')).values('image')[:1]))
+    print(followers[0].creator_profile_image)
+    return render(request, 'explore/followers.html', {"profile": profile, "followers": followers})
+
 
 def following(request, username):
     profile = User.objects.get(username=username)
-    following = follower.objects.filter(follower=profile)
+    followers = follower.objects.filter(follower=profile)
+    follower_users = [follower.followee for follower in followers]
+    profiles = Profile.objects.filter(user__in=follower_users)
+    following = followers.annotate(creator_profile_image=Subquery(profiles.filter(user=OuterRef('followee')).values('image')[:1]))
+    print(following[0].creator_profile_image)
     return render(request, 'explore/following.html', {"profile":profile, "following":following})
