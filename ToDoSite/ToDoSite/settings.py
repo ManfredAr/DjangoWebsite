40f7caf3dd0e6a697b3f8d7cf3dc8e88b2d43570
Manfred Araujo <manfred.araujo.2021@live.rhul.ac.uk>
2023-07-27 13:14:32 +0100

    setting debug to false

--- ToDoSite/ToDoSite/settings.py
+++ ToDoSite/ToDoSite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d*j^c1@2eg-!&yrv$q76%&6$p^nb8&3+l3geai0l77s-vcx+t4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
