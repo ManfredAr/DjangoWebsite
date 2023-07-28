9a19321e733eb8448f9556f9924b1cc68e553ae2
Manfred Araujo <manfred.araujo.2021@live.rhul.ac.uk>
2023-07-28 11:30:08 +0100

    The explore page now works
    The search bar allows the user to enter a username or a tagname and it
    will display the names of matching tags/username with links

--- ToDoSite/backend/profiles.py
+++ ToDoSite/backend/profiles.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect
 from account.models import Profile
-from account.forms import ChangeForm
 from account.models import follower
 from backend.Post import Post
 from django.contrib.auth.models import User
--- ToDoSite/explore/views.py
+++ ToDoSite/explore/views.py
@@ -2,11 +2,25 @@ from django.shortcuts import render
 from django.contrib.auth.models import User
 from backend.follows import follows
 from backend.profiles import profiles
+from post.models import post
+from account.models import Profile
+from django.db.models import Subquery, OuterRef
+
 
 # Create your views here.
 def explore(request):
-    usernames = User.objects.values_list('username', flat=True)
-    return render(request, 'explore/explore.html', {"username":usernames})
+    if request.method == "POST":
+        search = request.POST.get("query")
+        if search[0:1] == "#":
+            matching_tags = post.objects.filter(tag__icontains=search[1:]).values_list('tag', flat=True).distinct()
+            return render(request, 'explore/explore.html', {'tags': matching_tags, 'search':search, 'expect_Tag':True})
+        else:
+            matching_users = User.objects.filter(username__icontains=search)
+            profiles = Profile.objects.filter(user__in=matching_users)
+            matching_users = matching_users.annotate(p_image=Subquery(profiles.filter(user=OuterRef('id')).values('image')[:1]))
+            matching_users = matching_users.annotate(p_desc=Subquery(profiles.filter(user=OuterRef('id')).values('description')[:1]))
+            return render(request, 'explore/explore.html', {'users': matching_users, 'search':search, 'expect_User':True})
+    return render(request, 'explore/explore.html', {})
 
 def person(request, username):
     return profiles.getPersonProfile(request, username)
