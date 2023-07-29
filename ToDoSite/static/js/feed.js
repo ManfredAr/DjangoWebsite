58f829dae9c112cadb14947cf3b1f8b493aca9b8
Manfred Araujo <manfred.araujo.2021@live.rhul.ac.uk>
2023-07-29 12:45:05 +0100

    Added css for the comment popup

--- ToDoSite/static/js/feed.js
+++ ToDoSite/static/js/feed.js
@@ -140,31 +140,3 @@ document.addEventListener('DOMContentLoaded', function() {
     comment.classList.remove('disabled');
   });
 });
-
-
-document.addEventListener('DOMContentLoaded', function() {
-  document.getElementById('followForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-
-    const formData = new FormData(this);
-    s = document.getElementsByClassName("userId")[0].getAttribute("id");
-    formData.append('person_id', s);
-    console.log(formData.get('person_id'));
-
-    const xhr = new XMLHttpRequest();
-    xhr.open('POST', '/explore/follow-unfollow/'); 
-    xhr.setRequestHeader('X-CSRFToken', formData.get('csrfmiddlewaretoken'));
-    xhr.onreadystatechange = function() {
-      if (xhr.readyState === XMLHttpRequest.DONE) {
-        if (xhr.status === 200) {
-          console.error('Success');
-          location.reload();
-        } else {
-          console.error('Request failed:', xhr.status);
-        }
-      }
-    };
-    
-    xhr.send(formData);
-  });
-});
