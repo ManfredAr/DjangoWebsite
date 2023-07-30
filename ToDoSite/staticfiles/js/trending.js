d4a86f52089e187fd51978895741e1aa8c810606
Manfred Araujo <manfred.araujo.2021@live.rhul.ac.uk>
2023-07-30 11:57:27 +0100

    Changing the project secrets

--- ToDoSite/staticfiles/js/trending.js
+++ ToDoSite/staticfiles/js/trending.js
@@ -1,12 +1,57 @@
-function validateForm() {
-    var search = document.getElementById("query").value;
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('trendForm').addEventListener('submit', function(event) {
+      event.preventDefault();
 
-    if (search.trim() === "") {
-        document.getElementById("form-error").style.display = "block";
-        return false;
-    } else {
-        document.getElementById("postForm").submit();
-    }
+      const choiceValue = event.submitter.value;
+  
+      const formData = new FormData(this);
+      formData.append('button', choiceValue);
+  
+      const xhr = new XMLHttpRequest();
+      xhr.open('POST', '/topic/'); 
+      xhr.setRequestHeader('X-CSRFToken', formData.get('csrfmiddlewaretoken'));
+      xhr.onreadystatechange = function() {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                const responseData = JSON.parse(xhr.responseText);
+    
+                // Get the container where you want to display the tags
+                const tagContainer = document.getElementsByClassName('categories')[0];
+    
+                // Clear the current content
+                tagContainer.innerHTML = '';
+    
+                // Loop through the data and add the tags to the container
+                responseData.categories.forEach(category => {
+                    const tagElement = document.createElement('div');
+                    tagElement.classList.add('category');
+                    const tagButton = document.createElement('button');
+                    const tagLink = document.createElement('a');
+                    tagLink.href = `/topic/${category.tag}`;
+                    tagLink.textContent = `${category.tag} (${category.post_count} posts)`;
+                    tagButton.appendChild(tagLink);
+                    tagElement.appendChild(tagButton);
+                    tagContainer.appendChild(tagElement);
+                });
 
-    return true; 
-}
+                allTimeButton = document.getElementById("A");
+                lastHourButton = document.getElementById("H");
+
+                if (responseData.hour) {
+                    allTimeButton.style.backgroundColor  = 'white';
+                    lastHourButton.style.backgroundColor  = 'grey';
+                  } else {
+                    allTimeButton.style.backgroundColor  = 'grey';
+                    lastHourButton.style.backgroundColor  = 'white';
+                  }
+    
+                console.error('Success');
+          } else {
+            console.error('Request failed:', xhr.status);
+          }
+        }
+      };
+      
+      xhr.send(formData);
+    });
+  });
